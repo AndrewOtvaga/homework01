fa93ad4c53b05726cae7e6bb50c7442c90808a6e
Andrew <andrew.otvaga@mail.ru>
2023-10-05 22:13:00 +0300

    выполнено задание с math.random. Двумя способами, сегодня на работе было время- прочитал про возвратный метод ещё

--- src/homework3/MathRandom.java
+++ src/homework3/MathRandom.java
@@ -1,14 +1,18 @@
 package homework3;
 
-public class MathRandom {
-    public static void main(String[] args) {
-        // Генерируем случайное целое число из отрезка [10;500]
+public class MathRandom
+{
+    public static void main(String[] args)
+    {
+        // Генерируем случайное число в диапазоне [10;500]
         var randomNumber = (Math.random() * (500 - 10 + 1)) + 10;
 
         // Проверяем, попало ли число в интервал (25;200)
-        if (randomNumber > 25 && randomNumber < 200) {
+        if (randomNumber > 25 && randomNumber < 200)
+        {
             System.out.println("Сгенерированное число " + randomNumber + " попало в интервал (25;200).");
-        } else {
+        } else
+        {
             System.out.println("Сгенерированное число " + randomNumber + " не попало в интервал (25;200).");
         }
     }
