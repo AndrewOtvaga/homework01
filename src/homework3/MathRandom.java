acbe04713e9e4a925a2928e54766f2d8588fc77e
Andrew <andrew.otvaga@mail.ru>
2023-10-05 22:17:15 +0300

    выполнено 1 задание первый лишний файл удален

--- src/homework3/MathRandom.java
+++ src/homework3/MathRandom.java
@@ -4,14 +4,17 @@ public class MathRandom
 {
     public static void main(String[] args)
     {
-        // Генерируем случайное число в диапазоне [10;500]
+        // Перменная методом мас.рандом генерирует рандомное число в интервале [10;500]
         var randomNumber = (Math.random() * (500 - 10 + 1)) + 10;
 
         // Проверяем, попало ли число в интервал (25;200)
+        //Если попало
         if (randomNumber > 25 && randomNumber < 200)
         {
             System.out.println("Сгенерированное число " + randomNumber + " попало в интервал (25;200).");
-        } else
+        }
+        //Если не попало
+        else
         {
             System.out.println("Сгенерированное число " + randomNumber + " не попало в интервал (25;200).");
         }
