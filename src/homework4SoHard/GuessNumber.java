47be3a8c9b4c4a61aa7863e091a9db50ae3e2c98
Andrew <andrew.otvaga@mail.ru>
2023-10-08 03:01:26 +0300

    Задача с угадай числом, поигрался с while интересно так можно писать или нет, исправляю ошибки

---
+++ src/homework4SoHard/GuessNumber.java
@@ -0,0 +1,38 @@
+package homework4SoHard;
+
+import homework3.MathRandom;
+
+import java.util.Scanner;
+public class GuessNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int computerNumber = (int) (Math.random() * (9 - 1 + 1)) + 1);
+        while (1 > 0) //ради интереса написал проверить работает/ нет
+        {
+            System.out.println("Введите число (0 для выхода): ");
+            int input = scanner.nextInt();
+            if (input == 0)
+            {
+                System.out.println("Выход");
+                break;
+            }
+            else if (input == computerNumber)
+            {
+                System.out.println("Угадал, поздравляю!");
+                break;
+            }
+            else if (input > computerNumber)
+            {
+                System.out.println("Компьютер загадал меньше, попробуй снова");
+                break;
+            }
+            else  (input < computerNumber)
+            {
+                System.out.println("Компьютер загадал больше, попробуй снова");
+                break;
+            }
+        }
+    }
+}
