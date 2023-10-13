b2f47f2c4263de7be43944bda95c5238ba258b05
Andrew <andrew.otvaga@mail.ru>
2023-10-13 15:18:16 +0300

    всё решил, дополнение Вам в тг написал, крайнюю задачу что-то там насоображал, выдаёт фигню :( хотя урок пересмотрел и сделал в кальку под Вас, ткните носом, туплю(

--- src/homework5/Len.java
+++ src/homework5/Len.java
@@ -1,8 +1,38 @@
 package homework5;
 
-public class Len
-{
-    public static void main(String[] args) {
-//Пустой коммит
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+    public class Len
+    {
+        public static void main(String[] args)
+        {
+            Scanner scanner = new Scanner(System.in);
+
+            // Input the size of the array
+            System.out.print("Введите размер массива: ");
+            int len = scanner.nextInt();
+
+            // Create the array
+            int[] array = new int[len];
+
+            // Fill the array with random integers
+            Random random = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                array[i] = random.nextInt();
+            }
+
+            // Calculate the maximum, minimum, and average values
+            int max = Arrays.stream(array).max().getAsInt();
+            int min = Arrays.stream(array).min().getAsInt();
+            double average = Arrays.stream(array).average().getAsDouble();
+
+            // Display the results
+            System.out.println("Максимальное значение: " + max);
+            System.out.println("Минимальное значение: " + min);
+            System.out.println("Среднее арифметическое значение: " + average);
+        }
     }
-}
+
--- src/homework5/Window.java
+++ src/homework5/Window.java
@@ -2,34 +2,25 @@ package homework5;
 
 public class Window
 {
-        public static void main(String[] args)
-        {
-            int[] task04 = {88, 34, 12, 55, 90, 4, 10, 44};
-
-            // Инициализация начальных значений
-            int windowSize = 4;
-            int maxSum = Integer.MIN_VALUE;
-
-            // Обход массива с использованием скользящего окна
-            for (int i = 0; i <= task04.length - windowSize; i++)
+    public static void main(String[] args)
+    {
+        int[] task04 = {88, 34, 12, 55, 90, 4, 10, 44};
+        int target = 150;
+        int wStart = 0;
+        int arrSum = 0;
+        int arrLen = 10000;
+        for (int wEnd = 0; wEnd < task04.length; wEnd++)
             {
-                int currentSum = 0;
+               arrSum += task04[wEnd];
+               while (arrSum >= target)
+               {
+                   arrLen = Math.min(arrLen, wEnd - wStart + 1);
+                   arrSum -= task04[wStart];
+                   wStart++;
 
-                // Вычисление суммы смежных чисел в окне на данный момент
-                for (int k = i; k < i + windowSize; k++)
-                {
-                    currentSum += task04[k];
-                }
+               }
 
-                //  Значения максимальной суммы
-                if (currentSum > maxSum)
-                {
-                    maxSum = currentSum;
-                }
             }
-
-            //  Результат
-            System.out.println("Максимальная сумма четырех смежных чисел: " + maxSum);
-        }
+        System.out.println("Максимальная сумма четырех смежных чисел: " + arrLen);
+    }
 }
-
