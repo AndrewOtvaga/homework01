fab444e82ca91a8c740c80628d1235e266b4cb1d
Andrew <andrew.otvaga@mail.ru>
2023-10-14 20:13:53 +0300

    чёт вторая без библиотеки ээрей вообще тяжко, выдаёт ошибку, и особо не прёт(

--- src/homework6/WithoutArraysIsDifficult.java
+++ src/homework6/WithoutArraysIsDifficult.java
@@ -2,33 +2,33 @@ package homework6;
 /*
 public class WithoutArraysIsDifficult {
     public static void main(String[] args) {
-        String str1 = "строка";
-        String str2 = "торкса";
+        String str1 = "шкаф";
+        String str2 = "факш";
 
         boolean isAnagram = checkAnagram(str1, str2);
 
         if (isAnagram) {
-            System.out.println("Строки являются анаграммами.");
+            System.out.println("Строки - анаграммы.");
         } else {
-            System.out.println("Строки не являются анаграммами.");
+            System.out.println("Строки не анаграммы.");
         }
     }
 
     public static boolean checkAnagram(String str1, String str2) {
-        // Приводим строки к нижнему регистру и удаляем пробелы
+        // Привожу строки к нижнему регистру и удаляю пробелы
         str1 = str1.toLowerCase().replaceAll("\\s", "");
         str2 = str2.toLowerCase().replaceAll("\\s", "");
 
-        // Проверяем, имеют ли строки одинаковую длину
+        // Проверяю, имеют ли строки одинаковую длину
         if (str1.length() != str2.length()) {
             return false;
         }
 
-        // Создаем массивы для хранения частот символов
+        // Создаю массивы для хранения частот символов
         int[] charCount1 = new int[26];
         int[] charCount2 = new int[26];
 
-        // Подсчитываем частоты символов в первой строке
+        // Подсчитываю частоты символов в первой строке
         for (int i = 0; i < str1.length(); i++) {
             char c = str1.charAt(i);
             if (Character.isLetter(c)) {
@@ -36,7 +36,7 @@ public class WithoutArraysIsDifficult {
             }
         }
 
-        // Подсчитываем частоты символов во второй строке
+        // Подсчитываю частоты символов во второй строке
         for (int i = 0; i < str2.length(); i++) {
             char c = str2.charAt(i);
             if (Character.isLetter(c)) {
@@ -44,7 +44,7 @@ public class WithoutArraysIsDifficult {
             }
         }
 
-        // Сравниваем частоты символов
+        // Сравниваю частоты символов
         for (int i = 0; i < 26; i++) {
             if (charCount1[i] != charCount2[i]) {
                 return false;
