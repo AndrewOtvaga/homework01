231ae18e487d07dc896b92450e26152f45945042
Andrew <andrew.otvaga@mail.ru>
2023-10-15 22:18:45 +0300

    чистовики перекочевавшие с занятия и доделанные + котик, в целом ничего не дописывал, там максимально понятно про котика

--- src/com/company/project/homework7/Cat.java
+++ src/com/company/project/homework7/Cat.java
@@ -1,45 +1,56 @@
 package com.company.project.homework7;
 
-public class Cat {
+public class Cat
+{
+
     private String name;
     private int weight;
     private int numberOfWins;
 
-    public Cat() {
-        this.name = "Cнежок";
-        this.weight = 1;
-    }
-
-    public Cat(String name, int weight) {
+    public Cat(String name, int weight)
+    {
         setName(name);
         setWeight(weight);
     }
 
-    public void setName(String name) {
-        if (name != null && name.replace(" ", "").length() >= 3) {
+    public void setName(String name)
+    {
+        if (name != null && name.replace(" ", "").length() >= 3)
+        {
             this.name = name;
         }
     }
 
-    public void setWeight(int weight) {
-        if (weight >= 1 && weight < 9) {
+    public void setWeight(int weight)
+    {
+        if (weight >= 1 && weight < 9)
+        {
             this.weight = weight;
+        } else
+        {
+            throw new IllegalArgumentException("Weight must be between 1 and 8");
         }
     }
 
-    public int getNumberOfWins() {
+    public int getNumberOfWins()
+    {
         return numberOfWins;
     }
 
-    public boolean attack(Cat opponent) {
-        if (opponent == null) {
+    public boolean attack(Cat opponent)
+    {
+        if (opponent == null)
+        {
             throw new IllegalArgumentException("Opponent cannot be null");
         }
 
-        if (this.weight > opponent.weight) {
+        if (this.weight > opponent.weight)
+        {
             this.numberOfWins++;
             return true;
-        } else {
+        }
+        else
+        {
             opponent.numberOfWins++;
             return false;
         }
